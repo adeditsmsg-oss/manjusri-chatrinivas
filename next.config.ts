ae15bc00861fd61b3d0169611b5aba7cb1a8fc28
adeditsmsg-oss <adedits.msg@gmail.com>
2026-06-19 22:45:56 +0530

    Upload next.config.ts to main

---
+++ next.config.ts
@@ -0,0 +1,11 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  basePath: "/manjusri-chatrinivas",
+};
+
+export default nextConfig;
